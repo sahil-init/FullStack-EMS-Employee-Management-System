90f486f7c6d82de0a9cdae465fdd54892606fb5d
sahil-init <sahilshinde.work@gmail.com>
2026-05-28 16:12:39 +0530

    typo in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
           <Route path="/payslips" element={<Payslips />} />
           <Route path="/settings" element={<Settings />} />
         </Route>
-        <Route path="/print/payslips/:id" element={PrintPayslip} />
+        <Route path="/print/payslips/:id" element={<PrintPayslip />} />
 
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
