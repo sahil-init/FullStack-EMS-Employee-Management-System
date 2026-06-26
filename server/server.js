85093b3942e41dab71e9f343cfa985376f1eb48d
sahil-init <sahilshinde.work@gmail.com>
2026-06-26 18:16:19 +0530

    APIs created for employees, authentication, attendance, profile

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import multer from 'multer';
+
 import connectDB from './config/db.js';
+import authRouter from './routes/authRoutes.js';
+import employeesRouter from './routes/employeeRoutes.js';
+import profileRouter from './routes/profileRoutes.js';
+import attendanceRouter from './routes/attendanceRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,6 +19,10 @@ app.use(multer().none());
 
 // Routes
 app.get('/', (req, res) => res.send('Server is running'));
+appp.use('/api/auth', authRouter);
+appp.use('/api/employees', employeesRouter);
+appp.use('/api/profile', profileRouter);
+appp.use('/api/attendance', attendanceRouter);
 
 await connectDB();
 
